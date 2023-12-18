40bc100307739d3b570f32cbbd2bd434a31bf39b
Madusanka-mexxar <madusankaw@mexxar.com>
2023-12-18 13:42:25 +0530

    test

--- src/main/java/com/example/Test/TestApplication.java
+++ src/main/java/com/example/Test/TestApplication.java
@@ -3,10 +3,11 @@ package com.example.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController("/api/v1")
+@RequestMapping("/api/v1")
 public class TestApplication {
 
 	@GetMapping("/message")
